fbba010c24d015d58969b1742f600e7b0f61ec0e
bartek <taras_basket@wp.pl>
2023-10-05 20:38:24 +0200

    setting mondgoDB

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,48 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const Post = require("./models/post");
 
 const app = express();
 
-app.use("/api/posts", (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PATCH, DELETE, OPTIONS"
+	);
+	next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+	const post = new Post({ title: req.body.title, content: req.body.content });
+	console.log(post);
+	res.status(201).json({
+		message: "Post added successfully",
+	});
+});
+
+app.get("/api/posts", (req, res, next) => {
 	const posts = [
 		{
-			id: "saffsdfsd",
-			title: "new post",
-			content: "some content",
+			id: "fadf12421l",
+			title: "First server-side post",
+			content: "This is coming from the server",
 		},
 		{
-			id: "xxxsfdxcvx",
-			title: "new post 22144",
-			content: "some content and more",
+			id: "ksajflaj132",
+			title: "Second server-side post",
+			content: "This is coming from the server!",
 		},
 	];
 	res.status(200).json({
-		message: "posts fetched succesfully!",
+		message: "Posts fetched successfully!",
 		posts: posts,
 	});
 });
