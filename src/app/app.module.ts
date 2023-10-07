a74d952ae3914ac301f0d94b207100abb5841322
bartek <taras_basket@wp.pl>
2023-10-07 18:14:28 +0200

    working on routing and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
 	declarations: [
@@ -32,6 +33,7 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
 		MatToolbarModule,
 		MatExpansionModule,
 		HttpClientModule,
+		AppRoutingModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
