bb414c89a7b0ad53da9064a2dd119241abcbaa97
bartek <taras_basket@wp.pl>
2023-10-09 22:42:38 +0200

    working on updating posts on server

--- backend/app.js
+++ backend/app.js
@@ -65,6 +65,16 @@ app.get("/api/posts", (req, res, next) => {
 	});
 });
 
+app.get("/api/posts/:id", (req, res, next) => {
+	Post.findById(req.params.id).then((post) => {
+		if (post) {
+			res.status(200).json(post);
+		} else {
+			res.status(404).json({ message: "Post not found!" });
+		}
+	});
+});
+
 app.delete("/api/posts/:id", (req, res, next) => {
 	Post.deleteOne({ _id: req.params.id }).then((result) => {
 		console.log(result);
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -42,7 +42,10 @@ export class PostsService {
 	}
 
 	getPost(id: string) {
-		return { ...this.posts.find((p) => p.id === id) };
+		// return { ...this.posts.find((p) => p.id === id) };
+		return this.http.get<{ _id: string; title: string; content: string }>(
+			"http://localhost:3000/api/posts" + id
+		);
 	}
 
 	addPost(title: string, content: string) {
@@ -65,7 +68,13 @@ export class PostsService {
 		const post: Post = { id: id, title: title, content: content };
 		this.http
 			.put("http://localhost:3000/api/posts" + id, post)
-			.subscribe((response) => console.log(response));
+			.subscribe(() => {
+				const updatedPosts = [...this.posts];
+				const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
+				updatedPosts[oldPostIndex] = post;
+				this.posts = updatedPosts;
+				this.postsUpdated.next([...this.posts]);
+			});
 	}
 
 	deletePost(postId: string) {
