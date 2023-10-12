017c028a385309478d9d46f85677e2eaea51f5b5
bartek <taras_basket@wp.pl>
2023-10-12 23:10:16 +0200

    adding image controls

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -37,6 +37,7 @@ export class PostCreateComponent implements OnInit, OnDestroy {
 				validators: [Validators.required, Validators.minLength(3)],
 			}),
 			content: new FormControl(null, { validators: Validators.required }),
+			image: new FormControl(null, { validators: [Validators.required] }),
 		});
 		this.route.paramMap.subscribe((paramMap: ParamMap) => {
 			if (paramMap.has("postId")) {
@@ -77,6 +78,14 @@ export class PostCreateComponent implements OnInit, OnDestroy {
 			);
 		}
 		this.form.reset();
+	}
+
+	onImagePicked(event: Event) {
+		const file = (event.target as HTMLInputElement).files[0];
+		this.form.patchValue({ image: file });
+		this.form.get("image").updateValueAndValidity();
+		console.log(file);
+		console.log(this.form);
 	}
 
 	ngOnDestroy() {}
