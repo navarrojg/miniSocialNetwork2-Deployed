6699a7b3c3608fb81fa3150ae5610f8802173157
bartek <taras_basket@wp.pl>
2023-10-16 22:42:04 +0200

    fetching images on the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const postRoutes = require("./routes/posts");
 
@@ -20,6 +21,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
