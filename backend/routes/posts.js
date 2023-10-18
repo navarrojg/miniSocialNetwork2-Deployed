dd7d92a3afd1dca11523f0b4f82ac05ccdf2ef19
bartek <taras_basket@wp.pl>
2023-10-18 22:52:54 +0200

    adjusting backend pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -71,7 +71,13 @@ router.put(
 );
 
 router.get("", (req, res, next) => {
-	Post.find().then((documents) => {
+	const pageSize = +req.query.pageSize;
+	const currentPage = +req.query.page;
+	const postQuery = Post.find();
+	if (pageSize && currentPage) {
+		postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+	}
+	postQuery.then((documents) => {
 		console.log(documents);
 		res.status(200).json({
 			message: "Posts fetched successfully!",
