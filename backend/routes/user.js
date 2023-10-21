9d560b1893e8af779c93c2a4a7bc93caf73f09d8
bartek <taras_basket@wp.pl>
2023-10-21 20:20:45 +0200

    working on sending token to frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -29,6 +29,7 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", (req, res, next) => {
+	let fetchedUser;
 	User.findOne({ email: req.body.email })
 		.then((user) => {
 			if (!user) {
@@ -36,6 +37,7 @@ router.post("/login", (req, res, next) => {
 					message: "Auth failed!",
 				});
 			}
+			fetchedUser = user;
 			return bcrypt.compare(req.body.password, user.password);
 		})
 		.then((result) => {
@@ -45,10 +47,11 @@ router.post("/login", (req, res, next) => {
 				});
 			}
 			const token = jwt.sign(
-				{ email: user.email, userId: user._id },
+				{ email: fetchedUser.email, userId: fetchedUser._id },
 				"secret_this_should_be_longer",
 				{ expiresIn: "1h" }
 			);
+			res.status(200).json({ token: token });
 		})
 		.catch((err) => {
 			return res.status(401).json({
