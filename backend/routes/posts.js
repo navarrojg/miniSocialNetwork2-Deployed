7d0d4a8716f5e1f3543a159eedd3eeaafe5828bd
bartek <taras_basket@wp.pl>
2023-10-21 21:42:13 +0200

    setting middleware to protect routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Post = require("../models/post");
 const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
@@ -28,6 +29,7 @@ const storage = multer.diskStorage({
 
 router.post(
 	"",
+	checkAuth,
 	multer({ storage: storage }).single("image"),
 	(req, res, next) => {
 		const url = req.protocol + "://" + req.get("host");
@@ -50,6 +52,7 @@ router.post(
 
 router.put(
 	"/:id",
+	checkAuth,
 	multer({ storage: storage }).single("image"),
 	(req, res, next) => {
 		let imagePath = req.body.imagePath;
@@ -102,7 +105,7 @@ router.get("/:id", (req, res, next) => {
 	});
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
 	Post.deleteOne({ _id: req.params.id }).then((result) => {
 		console.log(result);
 		res.status(200).json({ message: "Post deleted!" });
