44c3a52e03e76544254570bd592c0bf3f65f78a7
bartek <taras_basket@wp.pl>
2023-10-21 19:21:01 +0200

    connecting angular to backend, creating authService, authDataModel

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "../auth.service";
 
 @Component({
 	selector: "app-signup",
@@ -7,9 +8,13 @@ import { NgForm } from "@angular/forms";
 	styleUrls: ["./signup.component.css"],
 })
 export class SignupComponent {
+	constructor(private authService: AuthService) {}
 	isLoading = false;
 
 	onSingUp(form: NgForm) {
-		console.log(form);
+		if (form.invalid) {
+			return;
+		}
+		this.authService.createUser(form.value.email, form.value.password);
 	}
 }
