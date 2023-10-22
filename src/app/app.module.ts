6ef3bd3fa8266af49c47481165982157ce518ff7
bartek <taras_basket@wp.pl>
2023-10-22 17:04:10 +0200

    adding token to authRequest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 
 import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from "@angular/material/card";
@@ -18,8 +18,9 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { AuthInteceptor } from "./auth/auth-interceptor";
 
 @NgModule({
 	declarations: [
@@ -27,8 +28,8 @@ import { SignupComponent } from './auth/signup/signup.component';
 		PostCreateComponent,
 		HeaderComponent,
 		PostListComponent,
-  LoginComponent,
-  SignupComponent,
+		LoginComponent,
+		SignupComponent,
 	],
 	imports: [
 		BrowserModule,
@@ -45,7 +46,9 @@ import { SignupComponent } from './auth/signup/signup.component';
 		ReactiveFormsModule,
 		MatPaginatorModule,
 	],
-	providers: [],
+	providers: [
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthInteceptor, multi: true },
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
