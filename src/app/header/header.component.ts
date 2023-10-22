35934be1db9d5c4e19a5793c92d18e32bd9e2eb7
bartek <taras_basket@wp.pl>
2023-10-22 18:16:48 +0200

    updating headerComponent

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
 	selector: "app-header",
 	templateUrl: "./header.component.html",
 	styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy {
+	private authListenerSubs: Subscription;
+	userIsAuthenitcated = false;
+	constructor(private authService: AuthService) {}
+
+	ngOnInit() {
+		this.authListenerSubs = this.authService
+			.getAuthStatusListener()
+			.subscribe((isAuthenticated) => {
+				this.userIsAuthenitcated = isAuthenticated;
+			});
+	}
+
+	ngOnDestroy(): void {
+		this.authListenerSubs.unsubscribe();
+	}
+}
