107bed56abc0024c83caf1bad4af3b07aa12405f
bartek <taras_basket@wp.pl>
2023-10-22 23:02:27 +0200

    setting localStorage settings in app

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -50,21 +50,68 @@ export class AuthService {
 				this.token = token;
 				if (token) {
 					const expiresInDuration = response.expiresIn;
-					this.tokenTimer = setTimeout(() => {
-						this.logout();
-					}, expiresInDuration * 1000);
+					this.setAuthTimer(expiresInDuration);
 					this.isAuthenticated = true;
 					this.authStatusListener.next(true);
+					const now = new Date();
+					const expirationDate = new Date(
+						now.getTime() + expiresInDuration * 1000
+					);
+					this.saveAuthData(token, expirationDate);
 					this.router.navigate(["/"]);
 				}
 			});
+	}
+
+	autoAuthUser() {
+		const authInformation = this.getAuthData();
+		if (!authInformation) {
+			return;
+		}
+		const now = new Date();
+		const expiressIn = authInformation.expirationDate.getTime() - now.getTime();
+		if (expiressIn > 0) {
+			this.token = authInformation.token;
+			this.isAuthenticated = true;
+			this.setAuthTimer(expiressIn / 1000);
+			this.authStatusListener.next(true);
+		}
 	}
 
 	logout() {
 		this.token = null;
 		this.isAuthenticated = false;
 		this.authStatusListener.next(false);
-		this.router.navigate(["/login"]);
 		clearTimeout(this.tokenTimer);
+		this.clearAuthData();
+		this.router.navigate(["/login"]);
+	}
+
+	private setAuthTimer(duration: number) {
+		this.tokenTimer = setTimeout(() => {
+			this.logout();
+		}, duration * 1000);
+	}
+
+	private saveAuthData(token: string, expirationData: Date) {
+		localStorage.setItem("token", token);
+		localStorage.setItem("expiration", expirationData.toISOString());
+	}
+
+	private clearAuthData() {
+		localStorage.removeItem("token");
+		localStorage.removeItem("expiration");
+	}
+
+	private getAuthData() {
+		const token = localStorage.getItem("token");
+		const expirationDate = localStorage.getItem("expiration");
+		if (!token || !expirationDate) {
+			return;
+		}
+		return {
+			token: token,
+			expirationDate: new Date(expirationDate),
+		};
 	}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { AuthService } from "../auth/auth.service";
 import { Subscription } from "rxjs";
 
-
 @Component({
 	selector: "app-header",
 	templateUrl: "./header.component.html",
@@ -11,11 +10,10 @@ import { Subscription } from "rxjs";
 export class HeaderComponent implements OnInit, OnDestroy {
 	private authListenerSubs: Subscription;
 	userIsAuthenitcated = false;
-	constructor(
-		private authService: AuthService,
-	) {}
+	constructor(private authService: AuthService) {}
 
 	ngOnInit() {
+		this.userIsAuthenitcated = this.authService.getIsAuth();
 		this.authListenerSubs = this.authService
 			.getAuthStatusListener()
 			.subscribe((isAuthenticated) => {
