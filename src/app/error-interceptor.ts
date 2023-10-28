ee45065675f2887b6257b38c262a50464348e452
bartek <taras_basket@wp.pl>
2023-10-28 21:06:42 +0200

    setting error dialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -11,12 +11,16 @@ import { ErrorComponent } from "./error/error.component";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private dialog: MatDialog){}
+	constructor(private dialog: MatDialog) {}
 
 	intercept(req: HttpRequest<any>, next: HttpHandler) {
 		return next.handle(req).pipe(
 			catchError((error: HttpErrorResponse) => {
-				this.dialog.open(ErrorComponent)
+				let errorMessage = "An unkown error occurred!";
+				if (error.error.message) {
+					errorMessage = error.error.message;
+				}
+				this.dialog.open(ErrorComponent, { data: { message: errorMessage } });
 				return throwError(error);
 			})
 		);
