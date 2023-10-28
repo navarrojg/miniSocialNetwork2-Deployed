80a23392e486094d3ae618d3bd30f9c9d2cf0c15
bartek <taras_basket@wp.pl>
2023-10-28 18:45:42 +0200

    handling signup and login error, turningoff spinner

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "../auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
 	selector: "app-signup",
 	templateUrl: "./signup.component.html",
 	styleUrls: ["./signup.component.css"],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit, OnDestroy {
 	constructor(private authService: AuthService) {}
 	isLoading = false;
+	private authStatusSub: Subscription;
+
+	ngOnInit() {
+		this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+			authStatus =>{
+				this.isLoading = false
+			}
+		);
+	}
 
 	onSingUp(form: NgForm) {
 		if (form.invalid) {
@@ -17,5 +27,9 @@ export class SignupComponent {
 		}
 		this.isLoading = true;
 		this.authService.createUser(form.value.email, form.value.password);
+	}
+
+	ngOnDestroy() {
+		this.authStatusSub.unsubscribe();
 	}
 }
