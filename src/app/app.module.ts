6b086e33962666e8232df6da6575dec12934dcac
bartek <taras_basket@wp.pl>
2023-10-29 18:58:19 +0100

    splitting modules. creating angularMaterialMod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,6 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 
-import { MatInputModule } from "@angular/material/input";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatDialogModule } from "@angular/material/dialog";
-
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
@@ -22,7 +13,8 @@ import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { AuthInteceptor } from "./auth/auth-interceptor";
 import { ErrorInterceptor } from "./error-interceptor";
-import { ErrorComponent } from './error/error.component';
+import { ErrorComponent } from "./error/error.component";
+import { AngularMaterialModule } from "./angular-material.module";
 
 @NgModule({
 	declarations: [
@@ -32,29 +24,21 @@ import { ErrorComponent } from './error/error.component';
 		PostListComponent,
 		LoginComponent,
 		SignupComponent,
-  ErrorComponent,
+		ErrorComponent,
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
 		BrowserAnimationsModule,
-		MatInputModule,
-		MatCardModule,
-		MatButtonModule,
-		MatToolbarModule,
-		MatExpansionModule,
-		MatProgressSpinnerModule,
 		HttpClientModule,
 		AppRoutingModule,
 		ReactiveFormsModule,
-		MatPaginatorModule,
-		MatDialogModule,
+		AngularMaterialModule,
 	],
 	providers: [
 		{ provide: HTTP_INTERCEPTORS, useClass: AuthInteceptor, multi: true },
 		{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 	],
 	bootstrap: [AppComponent],
-
 })
 export class AppModule {}
