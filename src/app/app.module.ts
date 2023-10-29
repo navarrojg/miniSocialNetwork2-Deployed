f38fe87f287e11ee1c488a065b26f530e78c3027
bartek <taras_basket@wp.pl>
2023-10-29 19:24:56 +0100

    splitting modules. creating postModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
-
 import { AppComponent } from "./app.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
-import { PostListComponent } from "./posts/post-list/post-list.component";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
@@ -15,13 +13,12 @@ import { AuthInteceptor } from "./auth/auth-interceptor";
 import { ErrorInterceptor } from "./error-interceptor";
 import { ErrorComponent } from "./error/error.component";
 import { AngularMaterialModule } from "./angular-material.module";
+import { PostsModule } from "./posts/posts.module";
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		PostCreateComponent,
 		HeaderComponent,
-		PostListComponent,
 		LoginComponent,
 		SignupComponent,
 		ErrorComponent,
@@ -32,8 +29,8 @@ import { AngularMaterialModule } from "./angular-material.module";
 		BrowserAnimationsModule,
 		HttpClientModule,
 		AppRoutingModule,
-		ReactiveFormsModule,
 		AngularMaterialModule,
+		PostsModule,
 	],
 	providers: [
 		{ provide: HTTP_INTERCEPTORS, useClass: AuthInteceptor, multi: true },
