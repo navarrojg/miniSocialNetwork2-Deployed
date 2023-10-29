d02cf2e96fa426d7ed244dcccb8def9b644fa20e
bartek <taras_basket@wp.pl>
2023-10-29 19:40:01 +0100

    splitting modules. creating AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-
 import { AppRoutingModule } from "./app-routing.module";
-import { LoginComponent } from "./auth/login/login.component";
-import { SignupComponent } from "./auth/signup/signup.component";
 import { AuthInteceptor } from "./auth/auth-interceptor";
 import { ErrorInterceptor } from "./error-interceptor";
 import { ErrorComponent } from "./error/error.component";
 import { AngularMaterialModule } from "./angular-material.module";
 import { PostsModule } from "./posts/posts.module";
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		LoginComponent,
-		SignupComponent,
-		ErrorComponent,
-	],
+	declarations: [AppComponent, HeaderComponent, ErrorComponent],
 	imports: [
 		BrowserModule,
-		FormsModule,
 		BrowserAnimationsModule,
 		HttpClientModule,
 		AppRoutingModule,
 		AngularMaterialModule,
 		PostsModule,
+		AuthModule,
 	],
 	providers: [
 		{ provide: HTTP_INTERCEPTORS, useClass: AuthInteceptor, multi: true },
